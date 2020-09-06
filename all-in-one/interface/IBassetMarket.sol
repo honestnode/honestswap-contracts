2f72fc22083f57608e5267455104daf6881b95b3
Rambo <rambo@extantfuture.com>
2020-09-07 00:03:35 +0800

    update interface

--- all-in-one/interface/IBassetMarket.sol
+++ all-in-one/interface/IBassetMarket.sol
@@ -1,12 +1,13 @@
 pragma solidity 0.5.16;
+pragma experimental ABIEncoderV2;
 
 interface IBassetMarket {
 
     function getBassetPrice(address _bAsset) external view
     returns (uint256 price, uint8 decimals);
 
-    function getBassetsPrice(address[] _bAssets) external view
-    returns (uint256[] prices, uint8[] decimals);
+    function getBassetsPrice(address[] calldata _bAssets) external view
+    returns (uint256[] memory prices, uint8[] memory decimals);
 
 //    function setBassetPrice(address _bAsset, uint256 _price) external;
 //    function setBassetPrices(address[] _bAssets, uint256[] _prices) external;
