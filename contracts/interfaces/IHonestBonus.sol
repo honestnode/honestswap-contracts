ba2cb47a13c170fba817623cf3c177ede83e0c7f
Peng Wan <wanpeng@mail.com>
2020-09-09 19:39:18 +0800

    feature: add IHonestBonus contract interface

---
+++ contracts/interfaces/IHonestBonus.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.0;
+
+interface IHonestBonus {
+
+    function hasBonus(address _bAsset) external view returns (bool);
+
+    function bonusOf(address _account) external view returns (uint256);
+
+    function totalBonus() external view returns (uint256);
+
+    function addBonus(address _account) external returns (uint256);
+
+    function claimRewards(address _account) external returns (uint256);
+}
