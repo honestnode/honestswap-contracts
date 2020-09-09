82a2e32887f520c61e2a3e0f68243a7edc404bc6
Peng Wan <wanpeng@mail.com>
2020-09-09 18:58:00 +0800

    feature: add hnt dummy contracts

---
+++ contracts/hnt/IHNTRewards.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.0;
+
+interface IHNTRewards {
+
+    function setToken(address _token) external onlyOwner;
+
+    function totalSavings() external view returns (uint256);
+
+    function totalRewards() external view returns (uint256);
+
+    function savingsOf(address _account) external view returns (uint256);
+
+    function rewardsOf(address _account) external view returns (uint256);
+
+    function sharesOf(address _account) external view returns (uint256);
+
+    function deposit(uint256 _amount) external returns (bool);
+
+    function redeem(uint256 _amount) external returns (bool);
+}
--- migrations/1_hasset_migration.js
+++
@@ -1,5 +0,0 @@
-const Hasset = artifacts.require("Hasset");
-
-module.exports = function(deployer) {
-  deployer.deploy(Hasset);
-};
