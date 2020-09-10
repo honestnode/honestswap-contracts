adaee07020e4375d991a839eca2540645a224853
Rambo <rambo@extantfuture.com>
2020-09-10 22:06:55 +0800

    del deprecated

--- deprecated/mock/yToken.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.5.0;
-
-interface yToken {
-    function deposit(uint256 _amount) external;
-
-    function withdraw(uint256 _shares) external;
-
-    function balance() external view returns (uint256);
-}
